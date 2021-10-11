d4eb900a30b1af03d7ad0f1d71ef213794d91e84
Henrique <henrique.multitech@gmail.com>
2021-10-11 18:22:11 -0300

    Teste Componente Menu Link

--- src/components/Menu/MenuLink/test.tsx
+++ src/components/Menu/MenuLink/test.tsx
@@ -1,9 +1,36 @@
 import MenuLink from ".";
+import { screen } from "@testing-library/react";
 import { renderTheme } from "utils/testRenderTheme";
 
 describe("<MenuLink />", () => {
   it("should render ", () => {
     const { container } = renderTheme(<MenuLink />);
     expect(container).toMatchSnapshot();
+  });
+  it("should render Navigation and links ", () => {
+    renderTheme(<MenuLink />);
+    const navigation = screen.getByRole("navigation", { hidden: true });
+    expect(navigation).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", {
+        name: /inicio/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("link", {
+        name: /projetos/i,
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", {
+        name: /sobre/i,
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", {
+        name: /contato/i,
+      })
+    ).toBeInTheDocument();
   });
 });
