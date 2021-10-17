499ace13c99ed0e0e91cf0f628c68f501de1533c
Henrique <henrique.multitech@gmail.com>
2021-10-17 15:43:34 -0300

    Pagina slug de posts implementadas

---
+++ src/pages/projects/[slug].tsx
@@ -0,0 +1,14 @@
+import { Container } from "@chakra-ui/react";
+const Slug = () => {
+  return (
+    <Container
+      maxW="full"
+      h="80vh"
+      bgGradient="linear(to-l,#00bf72 ,#008793 ,#004d7a , #051937)"
+    >
+      <Container maxW="container.md"></Container>
+    </Container>
+  );
+};
+
+export default Slug;
